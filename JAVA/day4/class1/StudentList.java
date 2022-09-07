5d04d13312c87b33ae36e0e6766dff690965be8f
rohit-chendur <107607055+rohit-chendur@users.noreply.github.com>
2022-09-07 13:54:27 +0530

    Update StudentList.java

--- JAVA/day4/class1/StudentList.java
+++ JAVA/day4/class1/StudentList.java
@@ -7,7 +7,7 @@ public class StudentList {
 	List<Student> students = new ArrayList<>();
 	public List<Student> getStudent() {
 		students.add(new Student(101, "Louis", 91));
-		students.add(new Student(102, "Taylor", 95));
+		students.add(new Student(102, "Liam", 95));
 		students.add(new Student(103, "Harry", 96));
 		students.add(new Student(104, "Zain", 84));
 		students.add(new Student(105, "Naill", 80));
